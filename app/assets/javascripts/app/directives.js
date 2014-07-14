1318474f48f8382ad9832ff8812730698d092125
Miguel <miguel@bitnami.com>
2014-07-13 20:09:04 -0700

    Directives

--- app/assets/javascripts/app/directives.js
+++ app/assets/javascripts/app/directives.js
@@ -23,3 +23,50 @@ direct_module.directive('userPanel', function() {
     }
   };
 });
+
+direct_module.directive('userFavorite', function($q, UserService, Favorite) {
+  return {
+    restrict: 'A',
+    scope: {
+      ngMovie: '='
+    },
+    templateUrl: '/templates/user_favorite.html',
+    link:
+      function($scope){
+         $scope.$on('user:unset', function() {
+           $scope.currentUser = null;
+         });
+
+         $scope.isFavorite = false;
+
+         $scope.addFavorite = function(movie) {
+           $scope.isFavorite = true;
+           UserService.currentUser().then(function(user) {
+             Favorite.createForUserAndMovie(user, movie);
+           });
+         };
+
+         $scope.removeFavorite = function(movie) {
+           $scope.isFavorite = false;
+           UserService.currentUser().then(function(user) {
+             Favorite.removeFavorite(user, movie); 
+           });
+         };
+        $scope.$watch('ngMovie', function(newValue, oldValue) {
+          if(newValue){
+            UserService.currentUser().then(function(user) { 
+              if(user) {
+                $scope.currentUser = user;
+                Favorite.isFavorite(user, $scope.ngMovie).then(
+                  function(isFavorite) { 
+                    $scope.isFavorite = isFavorite;
+                });
+              } else {
+                $scope.isFavorite = false;
+              }
+            });
+          }
+        });
+         }
+  };
+});
